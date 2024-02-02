ff369e4596d1104b307530ba0beb2a93c1b5c3a9
MomiSonowal <8880momis@gmail.com>
2024-02-02 12:26:14 +0530

    Reports Done,saved in local drive

--- src/main/java/com/practice/zooticketportal/service/TicketService.java
+++ src/main/java/com/practice/zooticketportal/service/TicketService.java
@@ -1,12 +1,15 @@
 package com.practice.zooticketportal.service;
 
+import com.practice.zooticketportal.entity.Ticket;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.http.ResponseEntity;
 
 import java.io.FileNotFoundException;
+import java.util.List;
 
 
 public interface TicketService {
+    List<Ticket> getAllTicket();
 
     ResponseEntity<String> exportReport(String format) throws FileNotFoundException, JRException;
 }
--- src/main/java/com/practice/zooticketportal/serviceimpl/TicketServiceImpl.java
+++ src/main/java/com/practice/zooticketportal/serviceimpl/TicketServiceImpl.java
@@ -6,6 +6,7 @@ import com.practice.zooticketportal.repositories.TicketRepository;
 import com.practice.zooticketportal.service.TicketService;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
@@ -17,28 +18,34 @@ import java.util.List;
 import java.util.Map;
 
 @Service
-class TicketServiceImpl implements TicketService {
-
+public class TicketServiceImpl implements TicketService {
+    @Autowired
     private TicketRepository ticketRepository;
+
+    @Override
+    public List<Ticket> getAllTicket() {
+        return ticketRepository.findAll();
+    }
+    @Override
     public ResponseEntity<String> exportReport(String format) throws FileNotFoundException, JRException {
         String path="C:\\Users\\HP1\\IdeaProjects\\reports";
-        List<Ticket> tickets= ticketRepository.findAll();//getAllStudents();
+        List<Ticket> ticket= ticketRepository.findAll();//getAllStudents();
         //Load file and compile it
         File file = ResourceUtils.getFile("classpath:tickets.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(tickets);
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(ticket);
         Map<String,Object> parameters = new HashMap<String, Object>();
         parameters.put("createdBy", "Admin");
 
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 
         if(format.equalsIgnoreCase("html")){
-            JasperExportManager.exportReportToHtmlFile(jasperPrint, path+"\\tickets.html");
-            return ResponseEntity.ok("Report generated successfully in HTML format. Path:" + path + "\\tickets.html");
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path+"\\ticket.html");
+            return ResponseEntity.ok("Report generated successfully in HTML format. Path:" + path + "\\ticket.html");
         }
         if(format.equalsIgnoreCase("pdf")){
-            JasperExportManager.exportReportToPdfFile(jasperPrint, path+"\\tickets.pdf");
-            return ResponseEntity.ok("Report generated successfully in PDF format. Path:" + path +"\\tickets.pdf");
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path+"\\ticket.pdf");
+            return ResponseEntity.ok("Report generated successfully in PDF format. Path:" + path +"\\ticket.pdf");
 
         }
         return ResponseEntity.badRequest().body("Invalid Report format specified.");
