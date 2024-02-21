18863ffdd664bb94a8a5b3ca11b6edc5234ac49c
SahidHussain98 <md100shm@gmail.com>
2024-02-21 14:12:49 +0530

    momi's ticket

--- src/main/java/com/practice/zooticketportal/repositories/FeesRepo.java
+++ src/main/java/com/practice/zooticketportal/repositories/FeesRepo.java
@@ -3,5 +3,12 @@ package com.practice.zooticketportal.repositories;
 import com.practice.zooticketportal.entity.Fees;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Map;
+
 public interface FeesRepo extends JpaRepository<Fees,Long> {
+
+
+    List<Fees> findByNationalityNationalityIdAndCategoryCategoryIdAndEstablishmentEstablishmentId(Long nationalityId, Long categoryId, Long establishmentId);
+
 }
