6d1da7a7bbb8960ac307168be4d3ed247455535d
MomiSonowal <8880momis@gmail.com>
2024-03-21 14:36:59 +0530

    NonWorkingDays

--- src/main/java/com/practice/zooticketportal/repositories/EstablishmentRepo.java
+++ src/main/java/com/practice/zooticketportal/repositories/EstablishmentRepo.java
@@ -4,8 +4,10 @@ package com.practice.zooticketportal.repositories;
 import com.practice.zooticketportal.entity.Establishment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EstablishmentRepo extends JpaRepository<Establishment,Long> {
     List<Establishment> findByStatus(boolean status);
+
 }
--- src/main/java/com/practice/zooticketportal/repositories/NonWorkingDaysRepo.java
+++ src/main/java/com/practice/zooticketportal/repositories/NonWorkingDaysRepo.java
@@ -3,6 +3,10 @@ package com.practice.zooticketportal.repositories;
 import com.practice.zooticketportal.entity.NonWorkingDays;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface NonWorkingDaysRepo extends JpaRepository<NonWorkingDays,Long> {
+    List<LocalDate> findNonWorkingDatesByEstablishment_EstablishmentId(Long establishmentId);
 }
 
